1513b5bdba800a7f4d07445a9817ce7e969343cc
Hipolito Sanchez <sanchezrkm@gmail.com>
2024-09-09 21:00:22 -0600

    Revise y modifique eliminado los cambios del ultimo commit

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,22 +1,9 @@
-import React, { useRef } from 'react';
+import React from 'react';
 
 export function Button() {
-  const divRef = useRef<HTMLDivElement>(null);
-
-  const handleButton = () => {
-    if (!divRef.current) return;
-    divRef.current.style.background = 'red';
-  };
   return (
-    <div
-      ref={divRef}
-      style={{
-        background: 'orange',
-        border: '1px solid gray',
-        padding: '4px',
-        borderRadius: '6px',
-      }}>
-      <button onClick={handleButton}>Button </button>
+    <div>
+      <button>Button new</button>
     </div>
   );
 }
