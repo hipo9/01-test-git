71c84495c51a0cb4c54cf5886c7256be25ea8e53
Hipolito Sanchez <sanchezrkm@gmail.com>
2024-09-09 20:44:03 -0600

    Estilos listo de Button.tsx

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,9 +1,22 @@
-import React from 'react';
+import React, { useRef } from 'react';
 
 export function Button() {
+  const divRef = useRef<HTMLDivElement>(null);
+
+  const handleButton = () => {
+    if (!divRef.current) return;
+    divRef.current.style.background = 'red';
+  };
   return (
-    <div>
-      <button>Button </button>
+    <div
+      ref={divRef}
+      style={{
+        background: 'orange',
+        border: '1px solid gray',
+        padding: '4px',
+        borderRadius: '6px',
+      }}>
+      <button onClick={handleButton}>Button </button>
     </div>
   );
 }
